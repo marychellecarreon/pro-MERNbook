744f02c2255665f6e279d41ab7da8d9c9b7b36a5
Mary Chelle Carreon <marychellecarreon@gmail.com>
2017-09-23 21:15:40 +0800

    added parsing data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
 const contentNode = document.getElementById('contents');
 
+const issues = [
+ {
+   id: 1, status: 'Open', owner: 'Ravan',
+   created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
+   title: 'Error in console when clicking Add',
+ },
+
+ {
+   id: 2, status: 'Assigned', owner: 'Eddie',
+   created: new Date('2016-08-16'), effort: 14,
+   completionDate: new Date('2016-08-30'),
+   title: 'Missing bottom border on panel',
+ },
+ ];
+
 class IssueList extends React.Component {
   render() {
     return (
@@ -7,7 +22,7 @@ class IssueList extends React.Component {
       <h1>Issue Tracker</h1>
       <IssueFilter />
       <hr />
-      <IssueTable />
+      <IssueTable issues={issues} />
       <hr />
       <IssueAdd />
       </div>
@@ -25,8 +40,23 @@ class IssueFilter extends React.Component {
 
 class IssueTable extends React.Component {
   render() {
+    const issueRows = this.props.issues.map(issue => <IssueRow
+    key={issue.id} issue={issue} />)
     return (
-      <div>This is a placeholder for a table of Issues.</div>
+      <table className="bordered-table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Status</th>
+            <th>Owner</th>
+            <th>Created</th>
+            <th>Effort</th>
+            <th>Completion Date</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>{issueRows}</tbody>
+        </table>
     )
   }
 }
@@ -38,4 +68,35 @@ class IssueAdd extends React.Component {
     )
   }
 }
+
+class IssueRow extends React.Component {
+  render() {
+    const issue = this.props.issue;
+    return (
+      <tr>
+         <td>{issue.id}</td>
+         <td>{issue.status}</td>
+         <td>{issue.owner}</td>
+         <td>{issue.created.toDateString()}</td>
+         <td>{issue.effort}</td>
+         <td>{issue.completionDate ?
+         issue.completionDate.toDateString() : ''}</td>
+         <td>{issue.title}</td>
+      </tr>
+    )
+  }
+}
+
+class BorderWrap extends React.Component {
+  render() {
+    const borderedStyle = {border: "1px solid silver", padding: 6};
+    return (
+      <div style={borderedStyle}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+
 ReactDOM.render(<IssueList />, contentNode);  //Render the component inside the content node
--- static/App.js
+++ static/App.js
@@ -1,5 +1,16 @@
 const contentNode = document.getElementById('contents');
 
+const issues = [{
+  id: 1, status: 'Open', owner: 'Ravan',
+  created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
+  title: 'Error in console when clicking Add'
+}, {
+  id: 2, status: 'Assigned', owner: 'Eddie',
+  created: new Date('2016-08-16'), effort: 14,
+  completionDate: new Date('2016-08-30'),
+  title: 'Missing bottom border on panel'
+}];
+
 class IssueList extends React.Component {
   render() {
     return React.createElement(
@@ -12,7 +23,7 @@ class IssueList extends React.Component {
       ),
       React.createElement(IssueFilter, null),
       React.createElement('hr', null),
-      React.createElement(IssueTable, null),
+      React.createElement(IssueTable, { issues: issues }),
       React.createElement('hr', null),
       React.createElement(IssueAdd, null)
     );
@@ -31,10 +42,59 @@ class IssueFilter extends React.Component {
 
 class IssueTable extends React.Component {
   render() {
+    const issueRows = this.props.issues.map(issue => React.createElement(IssueRow, {
+      key: issue.id, issue: issue }));
     return React.createElement(
-      'div',
-      null,
-      'This is a placeholder for a table of Issues.'
+      'table',
+      { className: 'bordered-table' },
+      React.createElement(
+        'thead',
+        null,
+        React.createElement(
+          'tr',
+          null,
+          React.createElement(
+            'th',
+            null,
+            'Id'
+          ),
+          React.createElement(
+            'th',
+            null,
+            'Status'
+          ),
+          React.createElement(
+            'th',
+            null,
+            'Owner'
+          ),
+          React.createElement(
+            'th',
+            null,
+            'Created'
+          ),
+          React.createElement(
+            'th',
+            null,
+            'Effort'
+          ),
+          React.createElement(
+            'th',
+            null,
+            'Completion Date'
+          ),
+          React.createElement(
+            'th',
+            null,
+            'Title'
+          )
+        )
+      ),
+      React.createElement(
+        'tbody',
+        null,
+        issueRows
+      )
     );
   }
 }
@@ -48,4 +108,61 @@ class IssueAdd extends React.Component {
     );
   }
 }
+
+class IssueRow extends React.Component {
+  render() {
+    const issue = this.props.issue;
+    return React.createElement(
+      'tr',
+      null,
+      React.createElement(
+        'td',
+        null,
+        issue.id
+      ),
+      React.createElement(
+        'td',
+        null,
+        issue.status
+      ),
+      React.createElement(
+        'td',
+        null,
+        issue.owner
+      ),
+      React.createElement(
+        'td',
+        null,
+        issue.created.toDateString()
+      ),
+      React.createElement(
+        'td',
+        null,
+        issue.effort
+      ),
+      React.createElement(
+        'td',
+        null,
+        issue.completionDate ? issue.completionDate.toDateString() : ''
+      ),
+      React.createElement(
+        'td',
+        null,
+        issue.title
+      )
+    );
+  }
+}
+
+class BorderWrap extends React.Component {
+  render() {
+    const borderedStyle = { border: "1px solid silver", padding: 6 };
+    return React.createElement(
+      'div',
+      { style: borderedStyle },
+      this.props.children
+    );
+  }
+}
+
 ReactDOM.render(React.createElement(IssueList, null), contentNode); //Render the component inside the content node
