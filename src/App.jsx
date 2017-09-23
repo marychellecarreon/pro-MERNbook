34d44d6899c825a928d5bc8795f689a4a5674f67
Mary Chelle Carreon <marychellecarreon@gmail.com>
2017-09-23 20:34:07 +0800

    adding react components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
 const contentNode = document.getElementById('contents');
 
-const continents = ['Africa','America','Asia','Australia','Europe'];
-const message = continents.map(c => `Hello ${c}!`).join(' ');
+class IssueList extends React.Component {
+  render() {
+    return (
+     <div>
+      <h1>Issue Tracker</h1>
+      <IssueFilter />
+      <hr />
+      <IssueTable />
+      <hr />
+      <IssueAdd />
+      </div>
+    );
+  }
+}
 
-const component = <p>{message}</p>;
-ReactDOM.render(component, contentNode);
+class IssueFilter extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for the Issue Filter.</div>
+    )
+  }
+}
+
+class IssueTable extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for a table of Issues.</div>
+    )
+  }
+}
+
+class IssueAdd extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for an Issue Add entry form.</div>
+    )
+  }
+}
+ReactDOM.render(<IssueList />, contentNode);  //Render the component inside the content node
--- static/App.js
+++ static/App.js
@@ -1,15 +1,51 @@
-'use strict';
+const contentNode = document.getElementById('contents');
 
-var contentNode = document.getElementById('contents');
+class IssueList extends React.Component {
+  render() {
+    return React.createElement(
+      'div',
+      null,
+      React.createElement(
+        'h1',
+        null,
+        'Issue Tracker'
+      ),
+      React.createElement(IssueFilter, null),
+      React.createElement('hr', null),
+      React.createElement(IssueTable, null),
+      React.createElement('hr', null),
+      React.createElement(IssueAdd, null)
+    );
+  }
+}
 
-var continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-var message = continents.map(function (c) {
-  return 'Hello ' + c + '!';
-}).join(' ');
+class IssueFilter extends React.Component {
+  render() {
+    return React.createElement(
+      'div',
+      null,
+      'This is a placeholder for the Issue Filter.'
+    );
+  }
+}
 
-var component = React.createElement(
-  'p',
-  null,
-  message
-);
-ReactDOM.render(component, contentNode);
+class IssueTable extends React.Component {
+  render() {
+    return React.createElement(
+      'div',
+      null,
+      'This is a placeholder for a table of Issues.'
+    );
+  }
+}
+
+class IssueAdd extends React.Component {
+  render() {
+    return React.createElement(
+      'div',
+      null,
+      'This is a placeholder for an Issue Add entry form.'
+    );
+  }
+}
+ReactDOM.render(React.createElement(IssueList, null), contentNode); //Render the component inside the content node
