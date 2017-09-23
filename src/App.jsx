2aaf500d0b2c49de27a45770fa56aa00617399f5
Mary Chelle Carreon <marychellecarreon@gmail.com>
2017-09-24 00:59:29 +0800

    created api

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,5 @@
 const contentNode = document.getElementById('contents');
 
-const issues = [
- {
-   id: 1, status: 'Open', owner: 'Ravan',
-   created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
-   title: 'Error in console when clicking Add',
- },
-
- {
-   id: 2, status: 'Assigned', owner: 'Eddie',
-   created: new Date('2016-08-16'), effort: 14,
-   completionDate: new Date('2016-08-30'),
-   title: 'Missing bottom border on panel',
- },
- ];
-
 class IssueList extends React.Component {
   constructor() {
     super();
@@ -27,32 +12,44 @@ class IssueList extends React.Component {
   }
 
   loadData() {
-    setTimeout(() => {
-       this.setState({ issues: issues });
-    }, 500);
-  }
+    fetch('/api/issues').then(response =>
+       response.json()
+    ).then(data => {
+       console.log("Total count of records:", data._metadata.total_count);
+       data.records.forEach(issue => {
+         issue.created = newDate(issue.created);
+         if (issue.completionDate)
+            issue.completionDate = new Date(issue.completionDate);
+       });
+       this.setState({ issues: data.records });
+    }).catch(err => {
+       console.log(err);
+    });
+    }
 
   createIssue(newIssue) {
-    const newIssues = this.state.issues.slice();
-    newIssue.id = this.state.issues.length + 1;
-    newIssues.push(newIssue);
-    this.setState({ issues: newIssues });
-  }
-
-
-   render() {
-      return (
-        <div>
-          <h1>Issue Tracker</h1>
-          <IssueFilter />
-          <hr />
-          <IssueTable issues={this.state.issues} />
-          <hr />
-          <IssueAdd createIssue={this.createIssue} />
-        </div>
-      );
-   }
-  }
+    fetch('/api/issues', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newIssue),
+    }).then(response => {
+      if (response.ok) {
+        response.json().then(updatedIssue => {
+          updatedIssue.created = new Date(updatedIssue.created);
+          if (updatedIssue.completionDate)
+            updatedIssue.completionDate = new Date(updatedIssue.completionDate);
+          const newIssues = this.state.issues.concat(updatedIssue);
+          this.setState({ issues: newIssues });
+        });
+      } else {
+         response.json().then(error => {
+           alert("Failed to add issue: " + error.message)
+         });
+      }
+      }).catch(err => {
+        alert("Error in sending data to server: " + err.message);
+      });
+    }
 
 class IssueFilter extends React.Component {
   render() {
